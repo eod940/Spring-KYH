2c94b69f7dc4a75b5b63551e485e073fa361c2a1
eod940 <eod940@gmail.com>
2022-02-23 00:46:41 +0900

    2022.02.22 TIL

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,27 @@ public class JpaMain {
 
         //code
         try {
-            Member member = new Member();
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            member.setId("Id_A");
+            Member member2 = new Member();
+            member2.setUsername("B");
 
+            Member member3 = new Member();
+            member3.setUsername("C");
 
             System.out.println("================");
-            em.persist(member);
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,13 +4,29 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
+    @Column(name = "name", nullable = false)
+    private String username;
+
     public Member() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Long getId() {
