0d77baf023d04f5f6a90493cd737f68d02b1f9b1
eod940 <eod940@gmail.com>
2022-02-25 02:05:27 +0900

    2022.02.24 TIL #2

--- jpashop_jpaBasic/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop_jpaBasic/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,4 +1,32 @@
-package jpabook.jpashop;
+package hellojpa;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //code
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
 }
